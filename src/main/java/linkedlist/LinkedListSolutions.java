f069c841ad31992aa5e169994d6fc7a43a84741f
tong-ruochen@hotmail.com <0183>
2019-12-30 00:01:20 +0800

    feat:FindKthToTail

--- src/main/java/linkedlist/LinkedListSolutions.java
+++ src/main/java/linkedlist/LinkedListSolutions.java
@@ -16,14 +16,20 @@ public class LinkedListSolutions {
     }
 
     public ListNode FindKthToTail(ListNode head,int k) {
+        if (head == null || k <= 0) {
+            return null;
+        }
         ListNode pivot = head;
-        while (k-- > 0) {
+        while (--k > 0) {
+            if (pivot.next == null) {
+                return null;
+            }
             pivot = pivot.next;
         }
         while (pivot.next != null) {
             pivot = pivot.next;
             head = head.next;
         }
-        return head.next;
+        return head;
     }
 }
